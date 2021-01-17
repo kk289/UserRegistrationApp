baabea12e47a62e199b8c426b6d874625a75afa0
kk289 <kevilkhadka@gmail.com>
2021-01-16 21:18:14 -0800

    Added registration process

--- src/main/java/com/example/userapp/AppController.java
+++ src/main/java/com/example/userapp/AppController.java
@@ -1,8 +1,12 @@
 package com.example.userapp;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -17,6 +21,13 @@ public class AppController {
 		model.addAttribute("user", new User());
 		
 		return "SignUp";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+		repo.save(user);
+		
+		return "registered_users";
 	}
 }
 	
