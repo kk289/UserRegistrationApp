5c0b11259a33afb82cc170b4c8871fbf956ce58f
kk289 <kevilkhadka@gmail.com>
2021-01-17 08:02:39 -0800

    Update Web Security

--- src/main/java/com/example/userapp/WebSecurityConfig.java
+++ src/main/java/com/example/userapp/WebSecurityConfig.java
@@ -1,7 +1,25 @@
 package com.example.userapp;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
 
 }
