095c855d993d5bdda0ed751b7db28f7f625f83a1
kk289 <kevilkhadka@gmail.com>
2021-01-16 18:28:15 -0800

    Created Registration form

--- src/main/java/com/example/userapp/AppController.java
+++ src/main/java/com/example/userapp/AppController.java
@@ -1,6 +1,7 @@
 package com.example.userapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +11,12 @@ public class AppController {
 	public String viewHomePage() {
 		return "index";
 	}
-
+	
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		
+		return "SignUp";
+	}
 }
+	
